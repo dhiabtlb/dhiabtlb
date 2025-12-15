467c7df206eeef06814492ec5fb5e9ab4b175616
dhia eddine ben taleb <bentaleb.dhiaeddine@esprit.tn>
2025-12-15 01:30:40 +0100

    Delete ecotrack/public_html directory

--- ecotrack/public_html/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Cache-Control: no-cache, no-store, must-revalidate');
-header('Pragma: no-cache');
-header('Expires: 0');
-
-require_once dirname(__DIR__) . '/app/bootstrap.php';
-
-$app = new App();
-$app->run();
