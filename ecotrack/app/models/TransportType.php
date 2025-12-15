84c9e6ac4a836df06aeb6988d76f171ba2fe395f
dhia eddine ben taleb <bentaleb.dhiaeddine@esprit.tn>
2025-12-15 01:30:10 +0100

    Delete ecotrack/app directory

--- ecotrack/app/models/TransportType.php
+++
@@ -1,8 +0,0 @@
-<?php
-class TransportType extends Model {
-    protected $table = 'transport_types';
-
-    public function getAll() {
-        return $this->fetchAll("SELECT * FROM transport_types ORDER BY name");
-    }
-}
--- ecotrack/app/models/WasteType.php
+++
@@ -1,8 +0,0 @@
-<?php
-class WasteType extends Model {
-    protected $table = 'waste_types';
-
-    public function getAll() {
-        return $this->fetchAll("SELECT * FROM waste_types ORDER BY name");
-    }
-}
